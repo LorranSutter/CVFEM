8e7c8fb6a83c0e25a7073950dcf238cdc1d9f340
Lorran Sutter <sutterlorran@gmail.com>
2020-05-13 22:01:31 -0400

    Translate code | formatting

--- cvfem.py
+++ cvfem.py
@@ -3,62 +3,64 @@ import math
 import numpy as np
 import matplotlib.pyplot as plt
 
-#--------- Escrito em Python 3 ---------#
-
-# Le o arquivo que contem os seguintes dados:
-#    Numero de nos
-#    Coordendadas dos nos
-#    Vetor contendo o numero de suportes por no
-#    Numero maximo de suportes por no
-#    Matriz de suportes
-#    Numero maximo de nos por contorno
-#    Vetor contendo o numero de nos por contorno
-#    Matriz de contorno
-
-
 def read_file(arq):
-    print('Iniciada a leitura do arquivo de entrada')
+    '''
+    Read the file containing the following data
+        - Number of nodes
+        - Nodes coordinates
+        - Array of number of supports by node
+        - Max number of supports by node
+        - Support matrix
+        - Max number of nodes by boundary
+        - Array of number of nodes by boundary
+        - Boundary matrix
 
-    # Abre o arquivo
+    '''
+    print('\nStarting input file reading')
+
+    # Open the file
     f = open(arq, 'r')
 
-    # Armazena o numero de nos
+    # Store number of nodes
     n = int(f.readline())
-    print('Numero total de nos: ', n)
+    print('Total number of nodes: ', n)
 
-    # Inicializacao dos vetores de coordenadas
+    # Init coordinates array
     x = np.zeros(n)
     y = np.zeros(n)
 
     for k in range(n):
         x[k], y[k] = list(map(float, f.readline().split()))
 
-    # Inicializacao do vetor que contem o numero de suportes por no
+    # Init the array that contains the number of supports by node
     n_s = np.zeros(n, dtype=np.int64)
 
     for k in range(n):
         n_s[k] = int(f.readline())
 
-    # Numero maximo de suportes por no
+    # Max number of supports by node
     num_suport = int(f.readline()) + 1
 
-    # Inicializacao da matriz de suportes
+    # Init support matrix
     S = np.zeros((n, num_suport))
 
-    print('Iniciada a criacao da matriz de suportes (' +
+    print('Started creating the support matrix (' +
           str(n) + ',' + str(num_suport) + ')')
     for k in range(n):
         S[k, :] = list(map(int, (f.readline().split())))
+    print('Finished creating the support matrix')
 
-    num_seg = int(f.readline())  # boundary segments
+    # Boundary segments
+    num_seg = int(f.readline())
 
     n_b = np.zeros(num_seg)
 
     for k in range(num_seg):
         n_b[k] = int(f.readline())
 
-    print('Iniciada a criacao da matriz de contorno (' +
+    print('Started creating boundary matrix (' +
           str(num_seg) + ',' + str(int(max(n_b))) + ')')
+
     # Global node number of j_th node on i_th boundary
     B = np.zeros((num_seg, int(max(n_b))))
 
@@ -66,57 +68,52 @@ def read_file(arq):
         aux = list(map(int, f.readline().split()))
         for w in range(len(aux)):
             B[k, w] = aux[w]
+    print('Finished creating the boundary matrix')
 
-    print('Finalizada a criacao da matriz de contorno')
     f.close()
-    print('Finilizada a leitura do arquivo')
+    print('Finished reading the file\n')
 
     return [n, x, y, n_s, num_suport, S, num_seg, n_b, B]
 
-# Define os coeficientes a_i e a_i,j
-
 
 def set_coefficients(n, x, y, vx, vy, S, n_s):
-    ap = np.zeros(n)  # Coeficiente do no da iteracao
-    a = np.zeros((n, num_suport))  # Coeficiente dos suportes do no da iteracao
-    V = np.zeros(n)  # Volume do VC
-    xL = np.zeros(3)  # Coordenada Local x dos 3 nos no elemento da iteracao
-    yL = np.zeros(3)  # Coordenada Local y dos 3 nos no elemento da iteracao
-    # Difusividade nodal Local dos 3 nos no elemento da iteracao
-    dif_nodal_L = np.zeros(3)
-    # Velocidade nodal Local x dos 3 nos no elemento da iteracao
-    vxL = np.zeros(3)
-    # Velocidade nodal Local y dos 3 nos no elemento da iteracao
-    vyL = np.zeros(3)
-    Vele = 0  # Volume do elemento da iteracao
-    V = np.zeros(n)  # Volume de controle do no da iteracao
-    # Derivada em x da funcao de forma dos 3 nos no elemento da iteracao
-    Nx = np.zeros(3)
-    # Derivada em y da funcao de forma dos 3 nos no elemento da iteracao
-    Ny = np.zeros(3)
-    dif_face = 0  # Difusividade no ponto medio da face
-    vx_face = 0  # Velocidade x no ponto medio da face
-    vy_face = 0  # Velocidade y no ponto medio da face
-    delta_x = 0  # Componente x normal da area na face
-    delta_y = 0  # Componente y normal da area na face
-    q_face = 0  # Fluxo de volume na face
+    '''
+    Defines the coefficients a_i and a_i,j
+    '''
+    ap = np.zeros(n)              # Iteration node coefficient
+    a = np.zeros((n, num_suport)) # Coefficient of iteration node supports
+    V = np.zeros(n)               # Volume in the CV
+    xL = np.zeros(3)              # Local coordinate x of the 3 nodes in the iteration element
+    yL = np.zeros(3)              # Local coordinate y of the 3 nodes in the iteration element
+    dif_nodal_L = np.zeros(3)     # Local nodal diffusivity of the 3 nodes in the iteration element
+    vxL = np.zeros(3)             # Local nodal velocity x of the 3 nodes in the iteration element
+    vyL = np.zeros(3)             # Local nodal velocity y of the 3 nodes in the iteration element
+    Vele = 0                      # Volume of the iteration element
+    V = np.zeros(n)               # Control volume of the iteration node
+    Nx = np.zeros(3)              # Derivate in x of the shape function of the 3 nodes in the iteration element
+    Ny = np.zeros(3)              # Derivate in y of the shape function of the 3 nodes in the iteration element
+    diff_face = 0                 # Diffusivity in the midpoint of the face
+    vx_face = 0                   # Velocity x in the midpoint of the face
+    vy_face = 0                   # Velocity y in the midpoint of the face
+    delta_x = 0                   # Face area normal component x
+    delta_y = 0                   # Face area normal component y
+    q_face = 0                    # Face volume flow
 
     for k in range(n):
         if np.mod(k, 100) == 0:
-            print('Iteracao: ', k, '\t Faltam: ', n-k)
+            print('Iteration: ', k, '\t To finish: ', n-k)
 
-        loopcount = int(n_s[k])  # Numero de suportes por no
-        print(k)
-        if S[k, n_s[k]] == 0:  # Detecta se eh contorno
+        loopcount = int(n_s[k])  # Number of supports by node
+        if S[k, n_s[k]] == 0:  # Detects if is a boundary
             loopcount = int(n_s[k]-1)
 
-        xL[0] = x[k]  # Coordenda Local x do no da iteracao
-        yL[0] = y[k]  # Coordenda Local y do no da iteracao
-        dif_nodal_L[0] = dif_nodal[k]  # Difusividade nodal do no da iteracao
+        xL[0] = x[k]                   # Local coordinate x of the iteration node
+        yL[0] = y[k]                   # Local coordinate y of the iteration node
+        dif_nodal_L[0] = dif_nodal[k]  # Nodal diffusivity in the iteration node
         vxL[0] = vx[k]
         vyL[0] = vy[k]
 
-        # No da iteracao e suportes
+        # Iteration node and supports
         #           k,w+2 0------0 k,w+1
         #               / |     /|
         #              /  |    / |
@@ -131,36 +128,36 @@ def set_coefficients(n, x, y, vx, vy, S, n_s):
         #          | /    | /
         #    k,w+4 0------0 k,w+5
 
-        # Loop para cada suporte do no da iteracao
+        # Loop for each iteration node support
         for w in range(loopcount):
-            # Itera em pares de nos suportes no sentido anti-horario
+            # Iterate in pairs of support nodes counterclockwise
             k1 = int(S[k, w])
             k2 = int(S[k, w+1])
 
-            # Coordendas locais dos nos suportes do no da iteracao
+            # Local coordinates of the supporting nodes of the iteration node
             xL[1] = x[k1-1]
             yL[1] = y[k1-1]
             xL[2] = x[k2-1]
             yL[2] = y[k2-1]
 
-            # Difusividade nodal Local dos nos suportes do no da iteracao
+            # Local nodal diffusivity of the nodes supporting the iteration node
             dif_nodal_L[1] = dif_nodal[k1-1]
             dif_nodal_L[2] = dif_nodal[k2-1]
 
-            # Velocidade nodal Local dos nos suportes do no da iteracao
+            # Local nodal speed of iteration node support nodes
             vxL[1] = vx[k1-1]
             vyL[1] = vy[k1-1]
             vxL[2] = vx[k2-1]
             vyL[2] = vy[k2-1]
 
-            # Volume do elemento da iteracao
+            # Volume of the iteration element
             Vele = (xL[1]*yL[2]-xL[2]*yL[1]-xL[0]*yL[2] +
                     xL[0]*yL[1]+yL[0]*xL[2]-yL[0]*xL[1])/2
 
-            # Contribuicao para o VC, usa-se em situacoes que ha fonte
+            # Contribution to CV, used in situations that have a source
             V[k] = V[k] + Vele/3
 
-            # Derivadas das funcoes de forma do elemento
+            # Derivates of the element shape functions
             Nx[0] = (yL[1]-yL[2])/(2*Vele)
             Nx[1] = (yL[2]-yL[0])/(2*Vele)
             Nx[2] = (yL[0]-yL[1])/(2*Vele)
@@ -170,75 +167,71 @@ def set_coefficients(n, x, y, vx, vy, S, n_s):
 
             # -------------------- FACE 1 --------------------
 
-            # Difusividade no ponto medio da face
-            dif_face = (5*dif_nodal_L[0] + 5 *
+            # Diffusivity at the midpoint of the face
+            diff_face = (5*dif_nodal_L[0] + 5 *
                         dif_nodal_L[1] + 2*dif_nodal_L[2])/12
 
-            # Velocidade no ponto medio da face
+            # Velocity at the midpoint of the face
             vx_face = (5*vxL[0] + 5*vxL[1] + 2*vxL[2])/12
             vy_face = (5*vyL[0] + 5*vyL[1] + 2*vyL[2])/12
 
-            # Componente normal da area na face
+            # Normal area component on the face
             delta_x = ((xL[0]+xL[1]+xL[2])/3) - ((xL[0]+xL[1])/2)
             delta_y = ((yL[0]+yL[1]+yL[2])/3) - ((yL[0]+yL[1])/2)
 
-            # Fluxo de volume na face
+            # Face volume flow
             q_face = vx_face*delta_y - vy_face*delta_x
 
-            # Coeficientes de difusao
-            ap[k] = ap[k] + dif_face*(-Nx[0] * delta_y + Ny[0] * delta_x)
-            a[k, w] = a[k, w] + dif_face*(Nx[1] * delta_y - Ny[1] * delta_x)
-            a[k, w+1] = a[k, w+1] + dif_face * \
+            # Diffusion coefficients
+            ap[k] = ap[k] + diff_face*(-Nx[0] * delta_y + Ny[0] * delta_x)
+            a[k, w] = a[k, w] + diff_face*(Nx[1] * delta_y - Ny[1] * delta_x)
+            a[k, w+1] = a[k, w+1] + diff_face * \
                 (Nx[2] * delta_y - Ny[2] * delta_x)
 
-            # Coeficientes de adveccao
+            # Advection coefficients
             ap[k] = ap[k] + max(q_face, 0)
             a[k, w] = a[k, w] + max(-q_face, 0)
 
             # -------------------- FACE 2 --------------------
 
-            # Difusividade no ponto medio da face
-            dif_face = (5*dif_nodal_L[0] + 2 *
+            # Diffusivity at the midpoint of the face
+            diff_face = (5*dif_nodal_L[0] + 2 *
                         dif_nodal_L[1] + 5*dif_nodal_L[2])/12
 
-            # Velocidade no ponto medio da face
+            # Velocity at the midpoint of the face
             vx_face = (5*vxL[0] + 2*vxL[1] + 5*vxL[2])/12
             vy_face = (5*vyL[0] + 2*vyL[1] + 5*vyL[2])/12
 
-            # Componente normal da area na face
+            # Normal area component on the face
             delta_x = ((xL[0]+xL[2])/2) - ((xL[0]+xL[1]+xL[2])/3)
             delta_y = ((yL[0]+yL[2])/2) - ((yL[0]+yL[1]+yL[2])/3)
 
-            # Fluxo de volume na face
+            # Face volume flow
             q_face = vx_face*delta_y - vy_face*delta_x
 
-            # Coeficientes de difusao
-            ap[k] = ap[k] + dif_face*(-Nx[0] * delta_y + Ny[0] * delta_x)
-            a[k, w] = a[k, w] + dif_face*(Nx[1] * delta_y - Ny[1] * delta_x)
-            a[k, w+1] = a[k, w+1] + dif_face * \
+            # Diffusion coefficients
+            ap[k] = ap[k] + diff_face*(-Nx[0] * delta_y + Ny[0] * delta_x)
+            a[k, w] = a[k, w] + diff_face*(Nx[1] * delta_y - Ny[1] * delta_x)
+            a[k, w+1] = a[k, w+1] + diff_face * \
                 (Nx[2] * delta_y - Ny[2] * delta_x)
 
-            # Coeficientes de adveccao
+            # Advection coefficients
             ap[k] = ap[k] + max(q_face, 0)
             a[k, w+1] = a[k, w+1] + max(-q_face, 0)
-
-        # endfor loopcount
 
         a[k, 0] = a[k, 0] + a[k, n_s[k]]
         a[k, n_s[k]] = 0
 
-    # endfor n
-
     return [a, ap, V]
 
-# Define os contornos
-
-
 def set_boundary(B, n_b):
+    '''
+    Define boundaries
+    '''
     BC = np.zeros(n)
     BB = np.zeros(n)
 
-    # Para este caso especifico
+    # For this specific case
     for k in range(int(n_b[0])):
         BC[int(B[2, k])-1] = 1e18
 
@@ -248,18 +241,18 @@ def set_boundary(B, n_b):
 
     return [BC, BB]
 
-# Define a fonte, para este caso
-
-
 def set_source():
+    '''
+    Defines the source, for this case
+    '''
     QC = np.zeros(n)
     QB = np.zeros(n)
     return [QC, QB]
 
 
-def solver(n, S, n_s, a, ap, BC, BB, QC, QB, maxIt, tolerancia):
+def solver(n, S, n_s, a, ap, BC, BB, QC, QB, maxIt, tolerance):
     '''
-    Equacao 5.28, Voller
+    Equation 5.28, Voller
     '''
     phi_old = np.zeros(n)
     phi_new = np.zeros(n)
@@ -267,17 +260,16 @@ def solver(n, S, n_s, a, ap, BC, BB, QC, QB, maxIt, tolerancia):
 
     for it in range(maxIt):
         if(np.mod(it, 100) == 0):
-            print('Solver, iteracao: ', it, '\t Faltam: ', maxIt-it)
+            print('Solver, iteration: ', it, '\t To finish: ', maxIt-it)
         for k in range(n):
             RHS = BB[k] + QB[k]
             for w in range(int(n_s[k])):
                 RHS = RHS + a[k, w]*phi_new[int(S[k, w])-1]
             phi_new[k] = float(RHS/(ap[k]+BC[k]+QC[k]))
 
-        if(max(abs(phi_new-phi_old)) <= tolerancia):
+        if(max(abs(phi_new-phi_old)) <= tolerance):
             break
         phi_old = phi_new.copy()
-    # endfor it
 
     return phi_new
 
@@ -285,11 +277,11 @@ def solver(n, S, n_s, a, ap, BC, BB, QC, QB, maxIt, tolerancia):
 if __name__ == "__main__":
     arq = sys.argv[1]
     maxIt = 1500
-    tolerancia = 0.001
+    tolerance = 0.001
 
     [n, x, y, n_s, num_suport, S, num_seg, n_b, B] = read_file(arq)
 
-    # ------------ Parte, por hora, necessaria de conversao em coordenada polar ------------
+    # ------------ Convert to polar coordinates ------------
     radius = np.zeros(n)
     theta = np.zeros(n)
     for k in range(n):
@@ -305,23 +297,23 @@ if __name__ == "__main__":
     dif_nodal = np.zeros(n)
     for k in range(n):
         dif_nodal[k] = 1/math.sqrt(x[k]**2 + y[k]**2)
-    # -------------- Fim da parte necessaria de conversao em coordenada polar --------------
+    # --------- End of polar coordinates convertion --------
 
-    print('Iniciando o set dos coeficientes de adveccao e difusao')
+    print('Starting to set advection and diffusion coefficients')
     [a, ap, V] = set_coefficients(n, x, y, vx, vy, S, n_s)
-    print('Finalizado o set dos coeficientes de adveccao e difusao')
+    print('Finished to set advection and diffusion coefficients')
 
-    print('Iniciado o set das condicoes de contorno')
+    print('Starting to set boundary conditions')
     [BC, BB] = set_boundary(B, n_b)
-    print('Finalizado o set das condicoes de contorno')
+    print('Finished to set boundary conditions')
 
-    print('Iniciado o set da fonte')
+    print('Starting to set the source')
     [QC, QB] = set_source()
-    print('Finalizado o set da fonte')
+    print('Finished to set the source')
 
-    print('Iniciado o SOLVER')
-    phi = solver(n, S, n_s, a, ap, BC, BB, QC, QB, maxIt, tolerancia)
-    print('Finalizado o SOLVER')
+    print('\nStarting SOLVER')
+    phi = solver(n, S, n_s, a, ap, BC, BB, QC, QB, maxIt, tolerance)
+    print('Finished SOLVER\n')
 
     pout = 0
     xout = []
@@ -336,7 +328,7 @@ if __name__ == "__main__":
     for k in range(pout):
         ana[k] = (math.exp(xout[k]) - math.exp(2))/(math.exp(1) - math.exp(2))
 
-    plt.plot(xout, ana, 'ro', label='Analitico')
-    plt.plot(xout, phiout, 'bo', label='Numerico')
+    plt.plot(xout, ana, 'rd', label='Analitic')
+    plt.plot(xout, phiout, 'bo', label='Numerical')
     plt.legend(loc='best')
     plt.show()
